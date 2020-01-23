45387a3946a4d1dabec32fc7f97337b00ddaf564
akashgupta752000 <akashgupta752000@gmail.com>
2020-01-23 14:34:26 +0530

    Iteration updation changes

--- 36(LCM)/main.cpp
+++ 36(LCM)/main.cpp
@@ -2,12 +2,13 @@
 
 using namespace std;
     int lcm(int n1,int n2){
-        int x;
-        if((n1>n2)?x=n1:x=n2);
+        int x,xcopy;
+        x=(n1>n2)?n1:n2;
+	xcopy=x;
         while(x>0){
             if(x%n1==0&&x%n2==0)
                 return x;
-            x++;
+            x+=xcopy;
         }
     }
 int main()
@@ -15,5 +16,5 @@ int main()
    int n1,n2;
    cin>>n1>>n2;
    cout<<lcm(n1,n2);
-    return 0;
+   return 0;
 }
